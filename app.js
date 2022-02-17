c7a294b3b08f3af9de63c6951e7791e4ada0445d
FadhilRizky01 <Fadhilrizky231@gmail.com>
2022-02-17 21:05:45 +0700

    express task

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "ejs"); // register the template engine
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+  res.render("index", {
+    name: "fadhil",
+    age: 22,
+    hobby: "Gaming",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`contoh listen di port ${port}`);
+});
